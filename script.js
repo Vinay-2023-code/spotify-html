99fea3e9cb21757bf36dcb1a87699a7c11781e40
Vinay-2023-code <vinayrajbhar2023@gmail.com>
2024-01-15 21:59:26 +0530

    spotify -html css javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Welcome to spotify");
+let songs = [
+    {songName: "salam-e-Ishq", filePath: "song/1.mp3", coverPath: "cover/1.jpg"},
+    {songName: "salam-e-Ishq", filePath: "song/1.mp3", coverPath: "cover/1.jpg"},
+    {songName: "salam-e-Ishq", filePath: "song/1.mp3", coverPath: "cover/1.jpg"},
+    {songName: "salam-e-Ishq", filePath: "song/1.mp3", coverPath: "cover/1.jpg"},
+    {songName: "salam-e-Ishq", filePath: "song/1.mp3", coverPath: "cover/1.jpg"},
+    {songName: "salam-e-Ishq", filePath: "song/1.mp3", coverPath: "cover/1.jpg"},
+    {songName: "salam-e-Ishq", filePath: "song/1.mp3", coverPath: "cover/1.jpg"},
+]
+let audioElement = new Audio('1.mp3');
+// audioElement.play();
