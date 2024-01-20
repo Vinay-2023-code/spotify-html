89972e5d9351a9b4b796bd046e8cedcab6506d13
Vinay-2023-code <vinayrajbhar2023@gmail.com>
2024-01-20 09:51:03 +0530

    added changes

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 console.log("Welcome to spotify");
-let songs = [
-    {songName: "salam-e-Ishq", filePath: "song/1.mp3", coverPath: "cover/1.jpg"},
-    {songName: "salam-e-Ishq", filePath: "song/1.mp3", coverPath: "cover/1.jpg"},
-    {songName: "salam-e-Ishq", filePath: "song/1.mp3", coverPath: "cover/1.jpg"},
-    {songName: "salam-e-Ishq", filePath: "song/1.mp3", coverPath: "cover/1.jpg"},
-    {songName: "salam-e-Ishq", filePath: "song/1.mp3", coverPath: "cover/1.jpg"},
-    {songName: "salam-e-Ishq", filePath: "song/1.mp3", coverPath: "cover/1.jpg"},
-    {songName: "salam-e-Ishq", filePath: "song/1.mp3", coverPath: "cover/1.jpg"},
-]
+// initialize the variables
+let songIndex = 0;
 let audioElement = new Audio('1.mp3');
+let masterPlay = document.getElementById('masterPlay')
+let myProgressBar = document.getElementById('myProgressBar')
+let songs = [
+    {songName: "Ram Siya Ram", filePath: "1.mp3", coverPath: "cover/1.jpg"},
+    {songName: "salam-e-Ishq", filePath: "2.m4r", coverPath: "cover/1.jpg"},
+    {songName: "salam-e-Ishq", filePath: "3.mp3", coverPath: "cover/1.jpg"},
+    {songName: "salam-e-Ishq", filePath: "4.mp3", coverPath: "cover/1.jpg"},
+    {songName: "salam-e-Ishq", filePath: "5.mp3", coverPath: "cover/1.jpg"},
+    {songName: "salam-e-Ishq", filePath: "6.mp3", coverPath: "cover/1.jpg"},
+    {songName: "salam-e-Ishq", filePath: "7.mp3", coverPath: "cover/1.jpg"},
+]
 // audioElement.play();
+
+
+// Handel Play/Pause click
+masterPlay.addEventListener('click',()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+    }
+})
+// Listen to Events
+document.addEventListener('timeupdate',()=>{
+    console.log('timeupdate');
+    //Update Seekbar
+})
