deb8eccd1cbeca892f34866ce5656e2fe30ee890
Vinay-2023-code <vinayrajbhar2023@gmail.com>
2024-01-20 10:42:32 +0530

    Added Changes

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let songIndex = 0;
 let audioElement = new Audio('1.mp3');
 let masterPlay = document.getElementById('masterPlay')
 let myProgressBar = document.getElementById('myProgressBar')
+let gif = document.getElementById('gif')
 let songs = [
     {songName: "Ram Siya Ram", filePath: "1.mp3", coverPath: "cover/1.jpg"},
     {songName: "salam-e-Ishq", filePath: "2.m4r", coverPath: "cover/1.jpg"},
@@ -22,6 +23,13 @@ masterPlay.addEventListener('click',()=>{
         audioElement.play();
         masterPlay.classList.remove('fa-circle-play');
         masterPlay.classList.add('fa-circle-pause');
+        gif.style.opacity =1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-circle-pause');
+        masterPlay.classList.add('fa-circle-play');
+        gif.style.opacity =0;
     }
 })
 // Listen to Events
