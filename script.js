244e386a5623d2bd231ae524af0392e2a254e097
Vinay-2023-code <vinayrajbhar2023@gmail.com>
2024-01-23 14:29:23 +0530

    Added Changes

--- script.js
+++ script.js
@@ -1,39 +1,44 @@
 console.log("Welcome to spotify");
 // initialize the variables
 let songIndex = 0;
-let audioElement = new Audio('1.mp3');
-let masterPlay = document.getElementById('masterPlay')
-let myProgressBar = document.getElementById('myProgressBar')
-let gif = document.getElementById('gif')
+let audioElement = new Audio("1.mp3");
+let masterPlay = document.getElementById("masterPlay");
+let myProgressBar = document.getElementById("myProgressBar");
+let gif = document.getElementById("gif");
 let songs = [
-    {songName: "Ram Siya Ram", filePath: "1.mp3", coverPath: "cover/1.jpg"},
-    {songName: "salam-e-Ishq", filePath: "2.m4r", coverPath: "cover/1.jpg"},
-    {songName: "salam-e-Ishq", filePath: "3.mp3", coverPath: "cover/1.jpg"},
-    {songName: "salam-e-Ishq", filePath: "4.mp3", coverPath: "cover/1.jpg"},
-    {songName: "salam-e-Ishq", filePath: "5.mp3", coverPath: "cover/1.jpg"},
-    {songName: "salam-e-Ishq", filePath: "6.mp3", coverPath: "cover/1.jpg"},
-    {songName: "salam-e-Ishq", filePath: "7.mp3", coverPath: "cover/1.jpg"},
-]
+  { songName: "Ram Siya Ram", filePath: "1.mp3", coverPath: "1.jpg" },
+  { songName: "Ram Naam se Jagmag Hai", filePath: "2.m4r", coverPath: "cover/1.jpg" },
+  { songName: "Patthar pe Raam Naam", filePath: "3.mp3", coverPath: "cover/1.jpg" },
+  { songName: "Ram Aynge", filePath: "4.mp3", coverPath: "cover/1.jpg" },
+  { songName: "Yug Ram Raj Ka Aa Gaya", filePath: "5.mp3", coverPath: "cover/1.jpg" },
+  { songName: "Sab Kuch Tera ", filePath: "6.mp3", coverPath: "cover/1.jpg" },
+  { songName: "Bharat Ka Baccha Baccha", filePath: "7.mp3", coverPath: "cover/1.jpg" },
+];
+
+
 // audioElement.play();
 
-
 // Handel Play/Pause click
-masterPlay.addEventListener('click',()=>{
-    if(audioElement.paused || audioElement.currentTime<=0){
-        audioElement.play();
-        masterPlay.classList.remove('fa-circle-play');
-        masterPlay.classList.add('fa-circle-pause');
-        gif.style.opacity =1;
-    }
-    else{
-        audioElement.pause();
-        masterPlay.classList.remove('fa-circle-pause');
-        masterPlay.classList.add('fa-circle-play');
-        gif.style.opacity =0;
-    }
-})
+masterPlay.addEventListener("click", () => {
+  if (audioElement.paused || audioElement.currentTime <= 0) {
+    audioElement.play();
+    masterPlay.classList.remove("fa-circle-play");
+    masterPlay.classList.add("fa-circle-pause");
+    gif.style.opacity = 1;
+  } else {
+    audioElement.pause();
+    masterPlay.classList.remove("fa-circle-pause");
+    masterPlay.classList.add("fa-circle-play");
+    gif.style.opacity = 0;
+  }
+});
 // Listen to Events
-document.addEventListener('timeupdate',()=>{
-    console.log('timeupdate');
-    //Update Seekbar
-})
+audioElement.addEventListener("timeupdate", () => {
+  progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
+  myProgressBar.value = progress;
+  //Update Seekbar
+});
+myProgressBar.addEventListener("change", () => {
+  audioElement.currentTime =
+    (myProgressBar.value * audioElement.duration) / 100;
+});
